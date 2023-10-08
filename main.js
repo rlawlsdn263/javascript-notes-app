40fcd268d920a137dbaeb0f6bcd9335adfc34ac9
rlawlsdn263 <rlawlsdn263@naver.com>
2023-10-08 20:42:08 +0900

    [JS] 노트 데이터가 담긴 객체 생성
    - '메모 추가하기'를 클릭하면 제목, 내용, 생성 날짜가 담긴 객체를 생성함
    
    addBtn에 클릭이벤트를 걸고, titleTag.value와 descTag.value를 사용해 각 input과 textarea에 있는 데이터에 접근했다.
    
    메모 생성일자를 기록하기 위해 Date 인스턴스를 생성해 연, 월, 일에 대한 정보를 각각 구했다. getMonth()는 사용시에 숫자를 반환하기 때문에 months 배열을 생성한 다음 인덱싱으로 정확한 월을 구할 수 있었다.
    
    noteInfo 객체를 만들어 제목, 내용, 생성 날짜에 대한 데이터를 담도록 했다.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const addBox = document.querySelector(".add-box"); // 메모추가 박스
 const popupBox = document.querySelector(".popup-box"); // 팝업 박스
 const closeIcon = popupBox.querySelector('header i'); // 팝업창 닫기 아이콘
- 
+
 addBox.addEventListener('click', () => {
   // 메모 추가를 클릭하면 팝업 박스에 .show가 추가
   popupBox.classList.add('show');
@@ -11,4 +11,37 @@ addBox.addEventListener('click', () => {
 closeIcon.addEventListener('click', () => {
   // 팝업창의 X버튼을 클릭하면 팝업 박스에 .show를 제거
   popupBox.classList.remove('show');
+})
+
+/* 메모 추가기능 */
+const addBtn = popupBox.querySelector('button'); //추가하기 버튼
+const titleTag = popupBox.querySelector('input'); //제목칸
+const descTag = popupBox.querySelector('textarea'); //내용칸
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+addBtn.addEventListener('click', (e) => {
+  // form 태그 새로고침 방지
+  e.preventDefault();
+
+  // 제목, 내용칸 변수 생성
+  let noteTitle = titleTag.value;
+  let noteDesc = descTag.value;
+
+  if(noteTitle || noteDesc) {
+    let dateObj = new Date();
+    let month = months[dateObj.getMonth()]; // 월
+    let day = dateObj.getDate(); // 일
+    let year = dateObj.getFullYear(); // 연
+
+    // 노트 데이터 객체 생성
+    let noteInfo = {
+      title: noteTitle,
+      description: noteDesc,
+      date: `${month} ${day}, ${year}`
+    }
+
+    console.log(noteInfo);
+  }
 })
